c39761ebabaa6a85a526f6333bd151250291003a
Danilo Ricardo Dias Pena <danilord.pena@gmail.com>
2017-09-21 10:19:45 -0300

    checkpoint commit

--- StringValidation/Strings+Utils.swift
+++ StringValidation/Strings+Utils.swift
@@ -13,7 +13,6 @@ extension String {
     
     // MARK: - Email Validation
     func isValidEmail(_ email: String) -> Bool {
-        // print("validate calendar: \(testStr)")
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
         
         let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
@@ -72,7 +71,6 @@ extension String {
     
     // MARK: - Password Validation
     func isValidPassword(_ password: String) -> Bool {
-        
         if password.characters.count == 0 {
             return false
         }
